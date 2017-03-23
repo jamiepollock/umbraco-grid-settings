deeb6aff74efec546f3c0c06b0452de6563dfc16
jamiepollock <jamie.does.umbraco@gmail.com>
2017-03-23 10:59:38 -0400

    Updated order & titles of background-repeat properties

--- src/Our.Umbraco.GridSettings.Web/App_Plugins/Our.Umbraco.GridSettings/editors/BackgroundRepeat/controller.js
+++ src/Our.Umbraco.GridSettings.Web/App_Plugins/Our.Umbraco.GridSettings/editors/BackgroundRepeat/controller.js
@@ -3,10 +3,10 @@
 
     var controller = function ($scope) {
         $scope.options = [
-            { value: 'repeat', title: 'Repeat' },
             { value: 'no-repeat', title: 'No repeat' },
-            { value: 'repeat-x', title: 'Repeat horizontally' },
-            { value: 'repeat-y', title: 'Repeat vertically' }
+            { value: 'repeat', title: 'Repeat horizontally & vertically' },
+            { value: 'repeat-x', title: 'Repeat horizontally only' },
+            { value: 'repeat-y', title: 'Repeat vertically only' }
         ];
     };
 
