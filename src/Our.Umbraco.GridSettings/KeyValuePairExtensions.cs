54394778ef4e1fb407f4a9e72e56c6d2861bd47d
jamiepollock <jamie.does.umbraco@gmail.com>
2017-04-14 13:31:17 -0400

    Added KeyValuePairExtensions for IsValid

---
+++ src/Our.Umbraco.GridSettings/KeyValuePairExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Our.Umbraco.GridSettings
+{
+    internal static class KeyValuePairExtensions
+    {
+        public static bool IsValid(this KeyValuePair<string, string> entry)
+        {
+            return (string.IsNullOrWhiteSpace(entry.Key) &&
+                    string.IsNullOrWhiteSpace(entry.Value))
+                    == false;
+        }
+    }
+}
