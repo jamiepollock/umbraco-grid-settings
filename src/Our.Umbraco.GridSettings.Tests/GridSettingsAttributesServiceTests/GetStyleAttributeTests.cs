d9e4c9ba2af0dda1b556dfee8515303e84abae6e
jamiepollock <jamie.does.umbraco@gmail.com>
2017-04-14 13:43:47 -0400

    Added GetStyleAttribute tests

---
+++ src/Our.Umbraco.GridSettings.Tests/GridSettingsAttributesServiceTests/GetStyleAttributeTests.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Our.Umbraco.GridSettings.Services;
+
+namespace Our.Umbraco.GridSettings.Tests.GridSettingsAttributesServiceTests
+{
+    public class GetStyleAttributeTests
+    {
+        private const string validJsonWithStylesAndConfig = @"{
+          ""styles"": {
+            ""background-position"": ""right top"",
+            ""background-repeat"": ""no-repeat""
+          }
+        }";
+
+        [Test]
+        public void GivenValidConfiguration_ReturnValidAttribute()
+        {
+            // Arrange
+            var contentItem = JObject.Parse(validJsonWithStylesAndConfig);
+            var service = new GridSettingsAttributesService();
+
+            // Act
+            var styleAttribute = service.GetStyleAttribute(contentItem);
+
+            // Assert
+            Assert.AreEqual("background-position:right top; background-repeat:no-repeat;", styleAttribute.Value);
+        }
+
+        [Test]
+        public void GivenNoConfiguration_ReturnInvalidAttribute()
+        {
+            // Arrange
+            var contentItem = new JObject();
+            var service = new GridSettingsAttributesService();
+
+            // Act
+            var styleAttribute = service.GetStyleAttribute(contentItem);
+
+            // Assert
+            Assert.IsFalse(styleAttribute.IsValid());
+        }
+    }
+}
