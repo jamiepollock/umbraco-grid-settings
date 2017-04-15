2361cd6d485d9645799dcc9e71487e4cd348c1d2
jamiepollock <jamie.does.umbraco@gmail.com>
2017-04-15 17:51:43 -0400

    Added StringConcat Value resolver tests and new StringConcatBehavior

--- src/Our.Umbraco.GridSettings/Resolvers/StringConcatGridSettingValueResolver.cs
+++ src/Our.Umbraco.GridSettings/Resolvers/StringConcatGridSettingValueResolver.cs
@@ -3,20 +3,36 @@ using System.Linq;
 
 namespace Our.Umbraco.GridSettings.Resolvers
 {
+    /// <summary>
+    /// A <see cref="IGridSettingsAttributeValueResolver"/> which concatenates found attributes together with a given token.
+    /// </summary>
     public class StringConcatGridSettingValueResolver : IGridSettingsAttributeValueResolver
     {
         private readonly string _separator;
+        private readonly StringConcatBehavior _concatBehavior;
 
-        public StringConcatGridSettingValueResolver(string separator = " ")
+        public StringConcatGridSettingValueResolver(string separator = " ", StringConcatBehavior concatBehavior = StringConcatBehavior.IgnoreNullOrWhiteSpace)
         {
             _separator = separator;
+            _concatBehavior = concatBehavior;
         }
 
         public string ResolveAttributeValue(KeyValuePair<string, IDictionary<string, string>> property)
         {
-            var values = property.Value.Select(x => x.Value.ToString());
+            var values = property.Value.Select(x => x.Value);
+
+            if (_concatBehavior == StringConcatBehavior.IgnoreNullOrWhiteSpace)
+            {
+                return string.Join(_separator, values.Where(x => string.IsNullOrWhiteSpace(x) == false));
+            }
 
             return string.Join(_separator, values);
         }
+    }
+
+    public enum StringConcatBehavior
+    {
+        IgnoreNullOrWhiteSpace,
+        RespectNullOrWhiteSpace
     }
 }
