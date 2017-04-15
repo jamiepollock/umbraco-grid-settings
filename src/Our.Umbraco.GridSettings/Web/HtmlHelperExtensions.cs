b0fac26b0844df0076166ca789e7c2751cdddcea
jamiepollock <jamie.does.umbraco@gmail.com>
2017-04-14 20:48:13 -0400

    Added HTMLHelper extension for rendering attributes

---
+++ src/Our.Umbraco.GridSettings/Web/HtmlHelperExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using Our.Umbraco.GridSettings.Services;
+using Our.Umbraco.GridSettings.ValueResolvers;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Our.Umbraco.GridSettings.Web
+{
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Renders the Settings &amp; Styles associated with the current <see cref="JObject"/>.
+        /// </summary>
+        /// <param name="helper">The <see cref="HtmlHelper"/> instance.</param>
+        /// <param name="contentItem">The <see cref="JObject"/> item which holds the configuration for Settings & Styles. This is typically an area or row in the Umbraco Grid.</param>
+        /// <param name="attributesResolver">Optional Attributes Resolver: this provides bespoke logic for grouping attributes together.</param>
+        /// <param name="attributeValueResolvers">Optional Attribute Value Resolver: Once attributes are grouped Attribute Value Resolvers can tailor the output of a given grouping.</param>
+        /// <param name="defaultAttributeValueResolver">Optional Default Attribute Value Resolver: If an <see cref="IGridSettingsAttributeValueResolver"/> can not be found for a specific property this <see cref="IGridSettingsAttributeValueResolver"/> is used insteads.</param>
+        /// <returns>A <see cref="MvcHtmlString" /> containing all the resolved attributes with their values.</returns>
+        public static MvcHtmlString RenderGridSettingAttributes(this HtmlHelper helper, JObject contentItem, IGridSettingsAttributesResolver attributesResolver = null, IDictionary<string, IGridSettingsAttributeValueResolver> attributeValueResolvers = null, IGridSettingsAttributeValueResolver defaultAttributeValueResolver = null)
+        {
+            var gridSettingsService = new GridSettingsAttributesService(attributesResolver, defaultAttributeValueResolver, attributeValueResolvers);
+
+            var attributes = gridSettingsService.GetAllAttributes(contentItem);
+
+            var output = new StringBuilder();
+            foreach (var attribute in attributes)
+            {
+                output.AppendFormat(" {0}=\"{1}\"", attribute.Key, helper.AttributeEncode(attribute.Value));
+            }
+
+            return new MvcHtmlString(output.ToString());
+        }
+    }
+}
